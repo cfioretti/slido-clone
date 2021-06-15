ba97d20f5a2a7165cc8f3a925f5e2954cc23cecb
Claudio Fioretti <c.fioretti@acmeitalia.it>
2021-06-16 00:29:13 +0200

    axios GET test with Strapi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  axios.get('http://localhost:1337/events/1').then(response => {
+    console.log(response);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
