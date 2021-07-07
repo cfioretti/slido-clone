ef65bd10c2c95a2710f2e8e42f8076d6a5caeef7
Claudio Fioretti <c.fioretti@acmeitalia.it>
2021-07-07 16:58:54 +0200

    testing endpoints

--- cypress/integration/events.spec.js
+++ cypress/integration/events.spec.js
@@ -1,10 +1,34 @@
-describe("Events list", () => {
-    it("Can list the events", () => {
-        cy.server();
-        cy.route({
-            url: "/events",
-            method: "GET",
-            response: { status: "OK", code: 200 }
-        });
-    });
+describe("Events API", () => {
+    const getItems = () =>
+        cy.request('/events')
+            .its('body')
+
+    const getFirst = item =>
+        cy.request('/events/1')
+
+    it('returns JSON', () => {
+        cy.request('/events')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json')
+    })
+
+    it('loads the initial items', () => {
+        getItems()
+            .its('body')
+            .should('have.length.greaterThan', 0)
+    })
+
+    it('returns valid objects', () => {
+        getItems()
+            .each(value =>
+                expect(value).to.have.all.keys('uid', 'title', 'description', 'start_at', 'end_at')
+            )
+    })
+
+    it('returns first object', () => {
+        getFirst(value =>
+            expect(value).to.have.all.keys('uid', 'title', 'description', 'start_at', 'end_at')
+        )
+    })
 });
