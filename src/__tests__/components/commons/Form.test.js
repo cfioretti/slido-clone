6eaa2847294bf357c2a5f84d62c513181f6c2b54
Licini <f.licini@acmeitalia.it>
2021-07-09 15:48:50 +0200

    Form test

--- src/__tests__/components/commons/Form.test.js
+++ src/__tests__/components/commons/Form.test.js
@@ -1,7 +1,23 @@
-import {screen, render} from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import Input from '../../../components/commons/Input';
+import Button from '../../../components/commons/Button';
+import Form from '../../../components/commons/Form';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 describe('Form component', () => {
+  it('form submit', () => {
+    const handleSubmit = jest.fn();
 
-// test button submit event
+    render(
+      <Form onSubmit={handleSubmit}>
+        <Input type='text' label='Username' id='username' />
+        <Input type='text' label='Password' id='password' />
+        <Button type='submit'>Sign in</Button>
+      </Form>
+    );
+    userEvent.click(screen.getByText(/Sign in/i));
+    expect(handleSubmit).toHaveBeenCalledTimes(1);
+  });
+  // test button submit event
 });
