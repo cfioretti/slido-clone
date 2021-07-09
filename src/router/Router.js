3197000f2b9d0a076ca92d29ba7b7570c705b99a
Licini <f.licini@acmeitalia.it>
2021-07-09 15:56:51 +0200

    Homepage

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,20 +1,22 @@
-import React           from 'react'
-import { createBrowserHistory } from "history";
-import {Route, Switch} from 'react-router-dom'
-import PrivateRoute from "./PrivateRoute";
-import Login from "../pages/Login";
-import NotFound from "../pages/NotFound";
-import SlidoClone from '../pages/SlidoClone';
+import React from 'react';
+import { createBrowserHistory } from 'history';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+import Homepage from '../pages/Homepage';
 
 export const history = createBrowserHistory();
 
 const Router = () => {
-    return <div>
-        <Switch>
-            <Route path={'/login'} component={Login}/>
-            <PrivateRoute exact path={'/'} component={SlidoClone}/>
-            <Route component={NotFound} />
-        </Switch>
+  return (
+    <div>
+      <Switch>
+        <Route path={'/login'} component={Login} />
+        <PrivateRoute exact path={'/'} component={Homepage} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
-}
-export default Router
+  );
+};
+export default Router;
